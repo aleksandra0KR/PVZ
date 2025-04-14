4527d9c395e166670278ccf79c925ebae6245b67
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2025-04-14 21:48:07 +0300

    fix: readme and tables

--- migrations/20250411185729_new_user_table.sql
+++ migrations/20250411185729_new_user_table.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TYPE role AS ENUM ('employee', 'moderator');
+CREATE TYPE user_role AS ENUM ('employee', 'moderator');
 
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    role role NOT NULL
+    role user_role NOT NULL
 );
 CREATE INDEX idx_users_email_password ON users (email, password);
 -- +goose StatementEnd
